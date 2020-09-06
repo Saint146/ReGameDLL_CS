eee6cb4accc60bbac2c119aa70437b0482cea746
Alexander Trifonov <saintforspam@gmail.com>
2020-09-07 01:05:33 +0500

    force bot to grab a knife if all enemies are dead and the bomb is ticking (part 2)

--- regamedll/dlls/bot/cs_bot_weapon.cpp
+++ regamedll/dlls/bot/cs_bot_weapon.cpp
@@ -435,6 +435,10 @@ void CCSBot::EquipBestWeapon(bool mustEquip)
 	if (!mustEquip && m_equipTimer.GetElapsedTime() < minEquipInterval)
 		return;
 
+	// check if there is anybody to shoot at
+	if (TheCSBots()->IsBombPlanted() && !GetEnemiesRemaining())
+		return;
+
 	CBasePlayerWeapon *pPrimary = static_cast<CBasePlayerWeapon *>(m_rgpPlayerItems[PRIMARY_WEAPON_SLOT]);
 	if (pPrimary)
 	{
